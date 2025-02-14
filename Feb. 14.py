c866f0af2eebb6200788a6812b9004a509890703
SofiaBernaldez <SofiaB653@stemcollegiate.ca>
2025-02-14 11:58:47 -0700

    Create Feb. 14.py
    What I learnt today

---
+++ Feb. 14.py
@@ -0,0 +1,1 @@
+I learnt that you could make wacky recipes which is pretty silly core. I also learnt that I could code words so whatever word they, put it'll show up and I think thats pretty cool too!
