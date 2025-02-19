2ea5e1da9de47321a9233ee64f075865681c1a1b
SofiaBernaldez <SofiaB653@stemcollegiate.ca>
2025-02-19 10:28:16 -0700

    Create Feb 19.py

---
+++ Feb 19.py
@@ -0,0 +1,1 @@
+I learnt how make an epic story! I wrote it about Mohamed, but then changed it since yeah. I also made learnt how to add colour which is pretty cool!!!! I like colour on most things. 
