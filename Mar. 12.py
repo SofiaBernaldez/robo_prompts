435c967d59c4b28c688d7a56a0039c1dfd10fe79
SofiaGB <SofiaB653@stemcollegiate.ca>
2025-03-12 14:08:51 -0600

    Create Mar. 12.py
    10 days of python- day 8

---
+++ Mar. 12.py
@@ -0,0 +1,18 @@
+I didn't know that we had to paste our code onto our github but I do have my codes in my design journal. I'M SO SORRY MS. TERKPER PLEASE SPARE ME: 
+But anyway's here's my code for day 8: 
+print("Wazzupp!! Welcome to your every day every week affirmation OR insult generator!")
+print()
+name= input("What is your name?")
+print()
+if name == "Mr. Jak" or name== "mr. jak":
+  day = input("What is thy day of thy week?")
+  if day == "monday" or day == "Monday":
+    print("It is going to be a bald Monday", name)
+    if day == "tuesday" or day == "Tuesday": 
+      print("You bring the baldness to everyone's day", name, "!")
+    if day == "wednesday" or day == "Wednesday":
+        print("I was looking at the mirror today, oh wait! It's just your bald head LOLZIES")
+    if day == "thursday" or day == "thursday":
+      print("You should win #3 most bald award, #3 since there are many bald people balder than YOU", name, "Which is lowkey kinda crazy, but you're still bald!")
+      if day == "friday" or day == "Friday":
+        print(name, "It's bald Friday!!!")
